7ed2d19c90913f9c874285a8271251706acf9b79
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-12-21 23:58:54 +0100

    [wip] upgrade to discord js 13

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
+const DiscordJS = require('discord.js');
+const { Intents } = require('discord.js');
+const voice = require('@discordjs/voice');
+const {
+  joinVoiceChannel,
+  createAudioResource,
+  createAudioPlayer,
+} = require('@discordjs/voice');
+
 const prefix = '-';
 
 const { botToken } = process.env;
@@ -8,24 +16,31 @@ const { botToken } = process.env;
 const ytdl = require('ytdl-core');
 const yts = require('yt-search');
 
-const client = new Discord.Client();
+const client = new DiscordJS.Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+});
+// const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Ready!');
+});
 client.login(botToken);
 
 const queue = new Map();
 
-client.once('ready', () => {
-  console.log('Ready!');
-});
+// client.once('reconnecting', () => {
+//   console.log('Reconnecting!');
+// });
 
-client.once('reconnecting', () => {
-  console.log('Reconnecting!');
-});
+// client.once('disconnect', () => {
+//   console.log('Disconnect!');
+// });
 
-client.once('disconnect', () => {
-  console.log('Disconnect!');
-});
-
-client.on('message', async (message) => {
+client.on('messageCreate', async (message) => {
   const tokens = message.content.split(' ');
   let command = tokens.shift();
 
@@ -34,32 +49,60 @@ client.on('message', async (message) => {
 
     // remove prefix from command
     command = command.substring(1);
-    // commands[command](message, tokens);
 
-    if (command == 'play' || command == 'p') {
+    if (command === 'play' || command === 'p') {
       execute(message, tokens, serverQueue);
-    } else if (command == 'skip' || command == 's') {
-      skip(message, serverQueue);
-    } else if (command == 'stop') {
-      stop(message, serverQueue);
-    } else {
-      message.channel.send('You need to enter a valid command!');
+    } else if (command === 'ping') {
+      message.channel.send({
+        content: 'pong',
+      });
     }
   }
 });
 
+// client.on('message', async (message) => {
+// const tokens = message.content.split(' ');
+// let command = tokens.shift();
+// if (!message.author.bot && command[0] == prefix) {
+//   const serverQueue = queue.get(message.guild.id);
+//   // remove prefix from command
+//   command = command.substring(1);
+//   // commands[command](message, tokens);
+//   if (command == 'play' || command == 'p') {
+//     execute(message, tokens, serverQueue);
+//   } else if (command == 'skip' || command == 's') {
+//     skip(message, serverQueue);
+//   } else if (command == 'stop') {
+//     stop(message, serverQueue);
+//   } else {
+//     message.channel.send('You need to enter a valid command!');
+//   }
+// }
+// });
+
 async function execute(message, tokens, serverQueue) {
+  // check if you are in a voice channel
   const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel)
+  if (!voiceChannel) {
     return message.channel.send(
-      'You need to be in a voice channel to play music!'
+      'You need to be in a voice channel to play music'
     );
+  }
+
+  // check if bot has premission to join vc
   const permissions = voiceChannel.permissionsFor(message.client.user);
   if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
     return message.channel.send(
       'I need the permissions to join and speak in your voice channel!'
     );
   }
+
+  // let connection = await voiceChannel.join();
+  // let connection = joinVoiceChannel({
+  //   channelId: message.member.voice.channel.id,
+  //   guildId: message.guild.id,
+  //   adapterCreator: message.guild.voiceAdapterCreator,
+  // });
 
   let songUrl = '';
   if (ytdl.validateURL(tokens[0])) {
@@ -72,7 +115,6 @@ async function execute(message, tokens, serverQueue) {
       songUrl = search.videos[0].url;
     }
   }
-
   const songInfo = await ytdl.getInfo(songUrl);
   const song = {
     title: songInfo.videoDetails.title,
@@ -88,15 +130,18 @@ async function execute(message, tokens, serverQueue) {
       volume: 5,
       playing: true,
     };
-
     queue.set(message.guild.id, queueContruct);
-
     queueContruct.songs.push(song);
-
     try {
-      let connection = await voiceChannel.join();
-      queueContruct.connection = connection;
-      play(message.guild, queueContruct.songs[0]);
+      // let connection = await voiceChannel.join();
+      let connection = joinVoiceChannel({
+        channelId: message.member.voice.channel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator,
+      });
+
+      // queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0], connection);
     } catch (err) {
       console.log(err);
       queue.delete(message.guild.id);
@@ -131,7 +176,7 @@ function stop(message, serverQueue) {
   serverQueue.connection.dispatcher.end();
 }
 
-async function play(guild, song) {
+async function play(guild, song, connection) {
   const serverQueue = queue.get(guild.id);
   if (!song) {
     serverQueue.voiceChannel.leave();
@@ -143,14 +188,24 @@ async function play(guild, song) {
   // let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
   const audio = await ytdl(song.url, { filter: 'audioonly' });
 
-  const dispatcher = serverQueue.connection
-    .play(audio)
-    .on('finish', () => {
-      serverQueue.songs.shift();
-      play(guild, serverQueue.songs[0]);
-    })
-    .on('error', (error) => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  const audioplayer = voice.createAudioPlayer();
+  // connection
+  // connection
+
+  audioplayer.play(createAudioResource(audio));
+  connection.subscribe(audioplayer);
+
+  // const dispatcher = serverQueue.connection
+  //   .subscribe(audioplayer)
+  //   .on('finish', () => {})
+  //   .on('error', (err) => {});
+  //   .play(audio)
+  //   .on('finish', () => {
+  //     serverQueue.songs.shift();
+  //     play(guild, serverQueue.songs[0]);
+  //   })
+  //   .on('error', (error) => console.error(error));
+  // dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
   // delete message after x seconds
   serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 }
