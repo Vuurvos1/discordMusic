9883c8e2c401f6753d251872228c6ace53d55b36
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-10-24 22:57:59 +0200

    adding type file

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+import { TextChannel, VoiceChannel, Message, Interaction } from 'discord.js';
+import { AudioPlayer } from '@discordjs/voice';
+
+// server queue, rename to servers?
+export type guildQueueItem = {
+	textChannel: TextChannel | null; // remove null?
+	voiceChannel: VoiceChannel | null; // remove null?
+	songMessage: Message | null;
+	connection: null; // get actual type from discord.js
+	player: AudioPlayer | null; // remove null?
+	songs: Songs; // create song type
+	volume: 5;
+	playing: false;
+	paused: false;
+	looping: false;
+};
+
+export type command = {
+	name: string;
+	description: string;
+	alias: string[];
+	interactionOptions: [];
+	permissions: { memberInVoice?: false };
+	command: function; // Message, string[], discord client
+	interaction: function; // Interaction, discord client
+};
+
+// song might not be the best name, maybe change to "Audio"
+export type Song = {
+	name: string;
+	platform: 'search' | 'youtube' | 'twitch' | 'spotify' | 'soundcloud';
+	url: string;
+	user: string; // discord js user
+	message: strign;
+};
+
+export type Songs = Song[];
