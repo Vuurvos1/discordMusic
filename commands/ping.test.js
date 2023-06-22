1bdf8679eba191468665b219789e6a35716f76e0
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-06-22 20:22:02 +0200

    add ping test

---
+++ commands/ping.test.js
@@ -0,0 +1,40 @@
+import { describe, it, expect, vi } from 'vitest';
+import command from './ping';
+
+describe('ping', () => {
+	const send = vi.fn();
+	const react = vi.fn();
+	const reply = vi.fn();
+
+	/** @type { unknown | import('discord.js').Message} */
+	const message = {
+		content: '',
+		author: {
+			bot: false
+		},
+		channel: {
+			send: send
+		},
+		react: react
+	};
+
+	it('should return pong (chat)', async () => {
+		// @ts-ignore
+		await command.command({ message });
+		expect(send).toHaveBeenCalledWith('pong!');
+	});
+
+	/** @type { unknown | import('discord.js').ChatInputCommandInteraction} */
+	const interaction = {
+		reply: reply
+	};
+
+	it('should return pong (interaction)', async () => {
+		// @ts-ignore
+		await command.interaction({ interaction });
+		expect(reply).toHaveBeenCalledWith({
+			content: 'pong!',
+			ephemeral: true
+		});
+	});
+});
