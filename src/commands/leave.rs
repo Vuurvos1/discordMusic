f7ea31de311f5242bc40e6543e1e3429aea5c642
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2025-05-27 08:31:31 +0200

    fix: skip and leave

--- src/commands/leave.rs
+++ src/commands/leave.rs
@@ -4,28 +4,28 @@ use crate::{check_msg, create_default_message, create_error_message, CommandResu
 #[poise::command(slash_command, guild_only)]
 pub async fn leave(ctx: Context<'_>) -> CommandResult {
     let guild_id = ctx.guild_id().unwrap();
-
     let manager = &ctx.data().songbird;
-    let has_handler = manager.get(guild_id).is_some();
 
-    if has_handler {
-        // Get the handler and clear the queue before leaving
-        // if let Some(handler_lock) = manager.get(guild_id) {
-        //     let handler = handler_lock.lock().await;
-        //     handler.queue().stop();
-        // }
-
-        if let Err(e) = manager.remove(guild_id).await {
-            let reply = create_error_message("Failed to leave voice channel".to_string());
+    let _handler_lock = match manager.get(guild_id) {
+        Some(handler) => handler,
+        None => {
+            let reply = create_error_message("Not in a voice channel".to_string());
             check_msg(ctx.send(reply).await);
-            println!("Failed to leave voice channel: {:?}", e);
             return Ok(());
         }
-    } else {
-        let reply = create_error_message("Not in a voice channel".to_string());
+    };
+
+    if let Err(e) = manager.remove(guild_id).await {
+        let reply = create_error_message("Failed to leave voice channel".to_string());
         check_msg(ctx.send(reply).await);
+        println!("Failed to leave voice channel: {:?}", e);
         return Ok(());
     }
+
+    // Clear the queue
+    let guilds_data_map_lock = ctx.data().guilds.clone();
+    let mut guilds_data_map = guilds_data_map_lock.lock().await;
+    guilds_data_map.remove(&guild_id.get());
 
     let reply = create_default_message("I've left the voice channel".to_string(), false);
     check_msg(ctx.send(reply).await);
--- src/commands/skip.rs
+++ src/commands/skip.rs
@@ -1,23 +1,38 @@
-use crate::{check_msg, create_default_message, create_error_message, CommandResult, Context};
+use crate::{
+    check_msg, create_default_message, create_error_message, utils::get_guild_data, CommandResult,
+    Context,
+};
 
 #[poise::command(slash_command, guild_only)]
 pub async fn skip(ctx: Context<'_>) -> CommandResult {
     let guild_id = ctx.guild_id().unwrap();
     let manager = &ctx.data().songbird;
 
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let handler = handler_lock.lock().await;
-        // let queue = handler.queue();
+    let _handler_lock = match manager.get(guild_id) {
+        Some(handler) => handler,
+        None => {
+            let reply = create_error_message("Not in a voice channel".to_string());
+            check_msg(ctx.send(reply).await);
+            return Ok(());
+        }
+    };
 
-        // if queue.skip().is_err() {
-        //     let reply = create_error_message("Failed to skip".to_string());
-        //     check_msg(ctx.send(reply).await);
-        //     return Ok(());
-        // }
-    } else {
-        let reply = create_error_message("Not in a voice channel".to_string());
+    let guild_data = get_guild_data(ctx, guild_id.get()).await;
+    let guild_data = guild_data.lock().await;
+
+    if guild_data.queue.is_empty() {
+        let reply = create_error_message("Queue is empty, nothing to skip".to_string());
         check_msg(ctx.send(reply).await);
         return Ok(());
+    }
+
+    if let Some(handler) = &guild_data.track_handle {
+        if let Err(e) = handler.stop() {
+            println!("Failed to skip song: {:?}", e);
+            let reply = create_error_message("Failed to skip song".to_string());
+            check_msg(ctx.send(reply).await);
+            return Ok(());
+        }
     }
 
     let reply = create_default_message("Skipped song".to_string(), false);
